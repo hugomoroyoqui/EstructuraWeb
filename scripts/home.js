4883150631b0f0f9b583148e1c45b763c9707745
Hugo Moroyoqui <hugo_moroyoqui@hotmail.com>
2022-06-10 17:35:01 +0100

    reCAPTCHA implementación

---
+++ scripts/home.js
@@ -0,0 +1,17 @@
+function getCaptchaResponse() {
+  var response = grecaptcha.getResponse();
+
+  if(response.length == 0) {
+    $("#g-recaptcha-error").show(0);
+  } else {
+    alert("Continuar con el envio del formulario");
+  }
+}
+
+function verifyCaptcha() {
+  $("#g-recaptcha-error").hide(0);
+}
+
+$("#submitForm").click(function() {
+  getCaptchaResponse();
+});
--- views/home.php
+++ views/home.php
@@ -1,2 +1,9 @@
-</br></br></br></br></br></br></br></br></br></br>
+<div class="g-recaptcha" data-sitekey="6LcekF4gAAAAACQcR6ZO1md1JBiNxKIWYzWS-aXl" data-callback="verifyCaptcha"></div>
+<p id="g-recaptcha-error" style="color:red; display: none;">Esta validación es requerida.</p>
+<a class="waves-effect waves-light btn" id="submitForm" style="width: 32%;">Enviar</a>
+
+</br></br></br></br></br></br></br>
+
+<script async src="https://www.google.com/recaptcha/api.js"></script>
+
 <script type="text/javascript" src="./scripts/home.js?v1.0.0"></script>
